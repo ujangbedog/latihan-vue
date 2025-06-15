9521dbc3626e7a127e8993e6c22f580ee4a5c7fe
ujangbedog <domba88galing@gmail.com>
2025-06-15 14:29:27 +0700

    chore(main): update import path for main.css after refactor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "@/App.vue";
 import router from "@/router";
-import "@/style.css";
+import "@/assets/main.css";
 import { setupToast } from "@/lib/toast";
 
 const app = createApp(App);
