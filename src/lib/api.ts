4631c00c0628c92a7002c0dbb1982cdebe8e7efc
ujangbedog <domba88galing@gmail.com>
2025-06-29 23:56:12 +0700

    chore: update API integration and routing to use mockAPI

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://api.vue.xpqx.xyz/v2";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://68615b7a8e7486408445b929.mockapi.io/api/v1";
 
 export const api = axios.create({
   baseURL: API_URL,
